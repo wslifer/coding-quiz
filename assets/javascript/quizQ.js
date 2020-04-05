a9d63de3067d4f6e1291c531b42a3f9a8730e887
Wesley Slifer <wesleyslifer@ymail.com>
2020-04-05 14:49:54 -0500

    adding leaderboard html and js file to store scores. Fixed js to show questions and display correct and incorrect for answers.

--- assets/javascript/quizQ.js
+++ assets/javascript/quizQ.js
@@ -1,27 +1,27 @@
 var quizQ = [
-    {
-        title: 'An array is surrounded by:',
-        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-        answer: 'square brackets'
-    },
-    {
-        title: 'The syntax for greater than or equal to is:',
-        choices: ['>>', '<=', '>=', '+='],
-        answer: '>='
-    },
-    {
-        title:'',
-        choices:[],
-        answer:''
-    },
-    {
-        title:'',
-        choices:[],
-        answer:''
-    },
-    {
-        title:'',
-        choices:[],
-        answer:''
-    },
+  {
+    title: "An array is surrounded by:",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "square brackets",
+  },
+  {
+    title: "The syntax for greater than or equal to is:",
+    choices: [">>", "<=", ">=", "+="],
+    answer: ">=",
+  },
+  {
+    title: "",
+    choices: [],
+    answer: "",
+  },
+  {
+    title: "",
+    choices: [],
+    answer: "",
+  },
+  {
+    title: "",
+    choices: [],
+    answer: "",
+  },
 ];
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,40 @@
-var startQuiz = document.querySelector('#startQuiz');
+var startQuiz = document.querySelector("#startQuiz");
+var quizBtn = document.querySelectorAll(".quizBtn");
 
 var score = 0;
 var currentIndex = 0;
 
-startQuiz.addEventListener('click', function(event){
-    event.stopPropagation();
-    /* startTimer(); */
-    document.querySelector('#boxOne').style.display = 'none';
-    document.querySelector('#boxTwo').style.display = 'block';
-    showQ(); 
+startQuiz.addEventListener("click", function (event) {
+  event.stopPropagation();
+  /* startTimer(); */
+  document.querySelector("#boxOne").style.display = "none";
+  document.querySelector("#boxTwo").style.display = "block";
+  showQ();
 });
 
-function showQ(){
-    var question = quizQ[currentIndex];
+function showQ() {
+  var question = quizQ[currentIndex];
 
-    document.querySelector('#title').innerHTML = question.title;
-    document.querySelector('#ansA').innerHTML = question.choices[0];
-    document.querySelector('#ansB').innerHTML = question.choices[1];
-    document.querySelector('#ansC').innerHTML = question.choices[2];
-    document.querySelector('#ansD').innerHTML = question.choices[3];
+  document.querySelector("#title").innerHTML = question.title;
+  document.querySelector("#ansA").innerHTML = question.choices[0];
+  document.querySelector("#ansB").innerHTML = question.choices[1];
+  document.querySelector("#ansC").innerHTML = question.choices[2];
+  document.querySelector("#ansD").innerHTML = question.choices[3];
 }
-for(var i = 0; i <quizBtn.length; i++){
-    quizBtn[i].addEventListener('click', function userChoice(event){
-        event.stopPropagation();
-        if(event.currentTarget.innerText === quizQ[currentIndex].answer){
-            score++;
-            console.log(score);
-            document.querySelector('#checkAns').innerHTML = 'Correct!';
-        }else {
-            document.querySelector('#checkAns').innerHTML = 'Incorrect!';
-            secondsleft = secondsleft - 20;
-        }
-        currentIndex++;
-        if(currentIndex < 5){
-            showQ();
-        }
-    })
+for (var i = 0; i < quizBtn.length; i++) {
+  quizBtn[i].addEventListener("click", function userChoice(event) {
+    event.stopPropagation();
+    if (event.currentTarget.innerText === quizQ[currentIndex].answer) {
+      score++;
+      console.log(score);
+      document.querySelector("#checkAns").innerHTML = "Correct!";
+    } else {
+      document.querySelector("#checkAns").innerHTML = "Incorrect!";
+      /* secondsleft = secondsleft - 20; */
+    }
+    currentIndex++;
+    if (currentIndex < 5) {
+      showQ();
+    }
+  });
 }
