d747624e7fdeb5341528b5c688592d153f0a1705
Wesley Slifer <wesleyslifer@ymail.com>
2020-04-05 15:14:10 -0500

    added vars to highscore page and started function to print highscore to page

---
+++ assets/javascript/leaderboard.js
@@ -0,0 +1,8 @@
+var highscores = JSON.parse(localStorage.getItem('highscores'));
+var leaderboard = document.querySelector('#leaderboardDisplay');
+
+window.addEventListener('load', function(){printHighscore()});
+
+function printHighscore(){
+    
+}
