17c6820a5716d9096bf3b1807d8335f6c46c4501
Wesley Slifer <wesleyslifer@ymail.com>
2020-04-05 15:26:10 -0500

    added function to place highscores in order

--- assets/javascript/leaderboard.js
+++ assets/javascript/leaderboard.js
@@ -4,5 +4,23 @@ var leaderboard = document.querySelector('#leaderboardDisplay');
 window.addEventListener('load', function(){printHighscore()});
 
 function printHighscore(){
-    
+    highscores = scoreSorter
+    for(var i = 0; i <highscores.length; i++){
+        var points = document.createElement('li');
+        var player = document.createTextNode(highscores[i].initials + ': ' + highscores[i].score);
+        points.appendChild(player);
+        leaderboard.appendChild(points);
+    };
+}
+
+
+//Looked this part up because I had in my pseudo code that I wanted scores to be sorted and had no clue how to do it. 
+function scoreSorter(array, key){
+    return array.sort(function(a,b){
+        if(a.score < b.score){
+            return 1;
+        }else{
+            return -1;
+        }
+    });
 }
